b68b01af6ea61b768d348168bbce92a6aec8a41a
dev-Leirbag <gabrielaf2186@gmail.com>
2026-05-12 00:09:06 -0300

    feat: Cria a tela principal do sistema

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,74 @@
-import database.model.TB_REPLICACAO_PROCESSO;
+import view.TelaReplicacaoProcessoTabelaView;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+import javax.swing.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Main extends JFrame {
+
+    private JDesktopPane desktop;
+    private static Connection conn;
+
+    public Main() {
+        setTitle("Sistema de Replicação de Dados");
+        setSize(1000, 700);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setResizable(false);
+
+        desktop = new JDesktopPane();
+        setContentPane(desktop);
+
+        JMenu menuSistema = new JMenu("Sistema");
+        JMenuItem itemExecutar = new JMenuItem("Executar Replicação");
+        itemExecutar.addActionListener(e -> {});
+        menuSistema.add(itemExecutar);
+
+        JMenuItem itemSair = new JMenuItem("Sair");
+        itemSair.addActionListener(e -> {
+            if(conn != null) {
+                try {
+                    conn.close();
+                } catch (SQLException ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
+            System.exit(0);
+        });
+        menuSistema.add(itemSair);
+
+        JMenu menuCadastro = new JMenu("Cadastro");
+        JMenuItem itemProcesso = new JMenuItem("Processo");
+        itemProcesso.addActionListener(e -> {});
+        menuCadastro.add(itemProcesso);
+
+        JMenuItem itemProcessoTabela = new JMenuItem("Processo X Tabelas");
+        itemProcessoTabela.addActionListener(e -> {});
+        menuCadastro.add(itemProcessoTabela);
+
+        JMenuItem itemDirecao = new JMenuItem("Direções");
+        itemDirecao.addActionListener(e -> {});
+        menuCadastro.add(itemDirecao);
+
+        JMenuBar menuBar = new JMenuBar();
+        menuBar.add(menuSistema);
+        menuBar.add(menuCadastro);
+        setJMenuBar(menuBar);
+    }
+
+
     public static void main(String[] args) {
 
-        TB_REPLICACAO_PROCESSO tbReplicacaoProcesso = new TB_REPLICACAO_PROCESSO();
-        tbReplicacaoProcesso.setId(1L);
-        tbReplicacaoProcesso.setProcesso("Completo");
-        tbReplicacaoProcesso.setDescricao("Processa todas as tabelas");
-        tbReplicacaoProcesso.setHabilitado(true);
-        System.out.println(tbReplicacaoProcesso);
+        try{
+            conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/controle", "postgres", "admin000");
+           SwingUtilities.invokeLater(() -> new Main().setVisible(true));
+        }catch (Exception e){
+            e.printStackTrace();
+            JOptionPane.showMessageDialog(null, "Erro ao conectar: " + e.getMessage());
+            System.exit(0);
+        }
+
+
     }
 }
